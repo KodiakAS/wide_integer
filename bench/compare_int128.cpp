cc34fa65fa24f0c0b81090a9605f271215acd21a
KodiakAS <38487204+KodiakAS@users.noreply.github.com>
2025-08-19 22:36:11 +0800

    Use system benchmark library

--- bench/compare_int128.cpp
+++ bench/compare_int128.cpp
@@ -1,6 +1,11 @@
 #include <benchmark/benchmark.h>
 #include <boost/multiprecision/cpp_int.hpp>
-#include <wide_integer/wide_integer.h>
+
+#ifdef USE_CXX11_HEADER
+#    include <wide_integer/wide_integer_cxx11.h>
+#else
+#    include <wide_integer/wide_integer.h>
+#endif
 
 using WInt128 = wide::integer<128, unsigned>;
 using WInt256 = wide::integer<256, unsigned>;
@@ -22,6 +27,21 @@ static void BM_Wide128Addition(benchmark::State & state)
     }
 }
 
+static void BM_Wide128Subtraction(benchmark::State & state)
+{
+    WInt128 a = 1234567890;
+    WInt128 b = 987654321;
+    for (auto _ : state)
+    {
+        WInt128 c = a;
+        for (int i = 0; i < 1000; ++i)
+        {
+            c -= b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
 static void BM_Builtin128Addition(benchmark::State & state)
 {
     unsigned __int128 a = 123456789;
@@ -32,6 +52,21 @@ static void BM_Builtin128Addition(benchmark::State & state)
         for (int i = 0; i < 1000; ++i)
         {
             c += b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
+static void BM_Builtin128Subtraction(benchmark::State & state)
+{
+    unsigned __int128 a = 1234567890;
+    unsigned __int128 b = 987654321;
+    for (auto _ : state)
+    {
+        unsigned __int128 c = a;
+        for (int i = 0; i < 1000; ++i)
+        {
+            c -= b;
             benchmark::DoNotOptimize(c);
         }
     }
@@ -52,6 +87,21 @@ static void BM_Wide128Multiplication(benchmark::State & state)
     }
 }
 
+static void BM_Wide128Division(benchmark::State & state)
+{
+    WInt128 a = 1234567890123456789ULL;
+    WInt128 b = 987654321;
+    for (auto _ : state)
+    {
+        WInt128 c = a;
+        for (int i = 0; i < 100; ++i)
+        {
+            c /= b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
 static void BM_Builtin128Multiplication(benchmark::State & state)
 {
     unsigned __int128 a = 123456789;
@@ -62,6 +112,21 @@ static void BM_Builtin128Multiplication(benchmark::State & state)
         for (int i = 0; i < 100; ++i)
         {
             c *= b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
+static void BM_Builtin128Division(benchmark::State & state)
+{
+    unsigned __int128 a = static_cast<unsigned __int128>(1234567890123456789ULL) << 64;
+    unsigned __int128 b = 987654321;
+    for (auto _ : state)
+    {
+        unsigned __int128 c = a;
+        for (int i = 0; i < 100; ++i)
+        {
+            c /= b;
             benchmark::DoNotOptimize(c);
         }
     }
@@ -82,6 +147,21 @@ static void BM_Boost128Addition(benchmark::State & state)
     }
 }
 
+static void BM_Boost128Subtraction(benchmark::State & state)
+{
+    BInt128 a = 1234567890;
+    BInt128 b = 987654321;
+    for (auto _ : state)
+    {
+        BInt128 c = a;
+        for (int i = 0; i < 1000; ++i)
+        {
+            c -= b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
 static void BM_Boost128Multiplication(benchmark::State & state)
 {
     BInt128 a = 123456789;
@@ -92,6 +172,21 @@ static void BM_Boost128Multiplication(benchmark::State & state)
         for (int i = 0; i < 100; ++i)
         {
             c *= b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
+static void BM_Boost128Division(benchmark::State & state)
+{
+    BInt128 a = 1234567890123456789ULL;
+    BInt128 b = 987654321;
+    for (auto _ : state)
+    {
+        BInt128 c = a;
+        for (int i = 0; i < 100; ++i)
+        {
+            c /= b;
             benchmark::DoNotOptimize(c);
         }
     }
@@ -112,6 +207,21 @@ static void BM_Wide256Addition(benchmark::State & state)
     }
 }
 
+static void BM_Wide256Subtraction(benchmark::State & state)
+{
+    WInt256 a = 1234567890;
+    WInt256 b = 987654321;
+    for (auto _ : state)
+    {
+        WInt256 c = a;
+        for (int i = 0; i < 1000; ++i)
+        {
+            c -= b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
 static void BM_Boost256Addition(benchmark::State & state)
 {
     BInt256 a = 123456789;
@@ -122,6 +232,21 @@ static void BM_Boost256Addition(benchmark::State & state)
         for (int i = 0; i < 1000; ++i)
         {
             c += b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
+static void BM_Boost256Subtraction(benchmark::State & state)
+{
+    BInt256 a = 1234567890;
+    BInt256 b = 987654321;
+    for (auto _ : state)
+    {
+        BInt256 c = a;
+        for (int i = 0; i < 1000; ++i)
+        {
+            c -= b;
             benchmark::DoNotOptimize(c);
         }
     }
@@ -142,6 +267,21 @@ static void BM_Wide256Multiplication(benchmark::State & state)
     }
 }
 
+static void BM_Wide256Division(benchmark::State & state)
+{
+    WInt256 a = WInt256{1234567890123456789ULL} << 128;
+    WInt256 b = 987654321;
+    for (auto _ : state)
+    {
+        WInt256 c = a;
+        for (int i = 0; i < 100; ++i)
+        {
+            c /= b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
 static void BM_Boost256Multiplication(benchmark::State & state)
 {
     BInt256 a = 123456789;
@@ -157,15 +297,40 @@ static void BM_Boost256Multiplication(benchmark::State & state)
     }
 }
 
+static void BM_Boost256Division(benchmark::State & state)
+{
+    BInt256 a = BInt256{1234567890123456789ULL} << 128;
+    BInt256 b = 987654321;
+    for (auto _ : state)
+    {
+        BInt256 c = a;
+        for (int i = 0; i < 100; ++i)
+        {
+            c /= b;
+            benchmark::DoNotOptimize(c);
+        }
+    }
+}
+
 BENCHMARK(BM_Wide128Addition);
+BENCHMARK(BM_Wide128Subtraction);
 BENCHMARK(BM_Builtin128Addition);
+BENCHMARK(BM_Builtin128Subtraction);
 BENCHMARK(BM_Wide128Multiplication);
+BENCHMARK(BM_Wide128Division);
 BENCHMARK(BM_Builtin128Multiplication);
+BENCHMARK(BM_Builtin128Division);
 BENCHMARK(BM_Boost128Addition);
+BENCHMARK(BM_Boost128Subtraction);
 BENCHMARK(BM_Boost128Multiplication);
+BENCHMARK(BM_Boost128Division);
 BENCHMARK(BM_Wide256Addition);
+BENCHMARK(BM_Wide256Subtraction);
 BENCHMARK(BM_Boost256Addition);
+BENCHMARK(BM_Boost256Subtraction);
 BENCHMARK(BM_Wide256Multiplication);
+BENCHMARK(BM_Wide256Division);
 BENCHMARK(BM_Boost256Multiplication);
+BENCHMARK(BM_Boost256Division);
 
 BENCHMARK_MAIN();
